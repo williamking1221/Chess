1060bcb00c278944605f1012a08b728115a6b02b
William Joshua King <wgk4@duke.edu>
2022-11-07 01:14:03 -0500

    Added move class. Can now play the game, but doesn't have special rules (en passant) and doens't check for valid moves.

--- Chess/chess_engine.py
+++ Chess/chess_engine.py
@@ -16,3 +16,32 @@ class GameState():
         ]
         self.white_to_move = True     # Check whose turn is it to move.
         self.move_log = []            # Move log
+
+    def make_move(self, move):
+        self.board[move.start_square_row][move.start_square_col] = "--"
+        self.board[move.end_square_row][move.end_square_col] = move.piece_moved
+        self.move_log.append(move)
+        self.white_to_move = not self.white_to_move
+
+class Move():
+    # Rank file notation
+    rank_to_row = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    row_to_rank = {v: k for k, v in rank_to_row.items()}
+    file_to_col = {"h": 7, "g": 6, "f": 5, "e": 4, "d": 3, "c": 2, "b": 1, "a": 0}
+    col_to_file = {v: k for k, v in file_to_col.items()}
+
+    def __init__(self, start_square, end_square, board):
+        self.start_square_row = start_square[0]
+        self.start_square_col = start_square[1]
+        self.end_square_row = end_square[0]
+        self.end_square_col = end_square[1]
+        self.piece_moved = board[self.start_square_row][self.start_square_col]
+        self.piece_capture = board[self.end_square_row][self.end_square_col]
+
+    # To make into real chess notation
+    def get_chess_not(self):
+        return self.get_rank_file(self.start_square_row, self.start_square_col) + \
+               self.get_rank_file(self.end_square_row, self.end_square_col)
+
+    def get_rank_file(self, r, c):
+        return self.col_to_file[c] + self.row_to_rank[r]
--- Chess/chess_main.py
+++ Chess/chess_main.py
@@ -27,17 +27,36 @@ def main():
     game_state = chess_engine.GameState()
     load_images()
     running = True
+    sq_selected = () # No square selected initially, track last square selected by user.
+    player_clicks = []  # Keep track of player clicks -- made of two tuples
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()    # Location of the mouse (x,y) coords
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sq_selected == (row, col):   # User clicked same square twice
+                    sq_selected = ()    # Reset square selected
+                    player_clicks = []  # Clear player clicks
+                else:
+                    sq_selected = (row, col)
+                    player_clicks.append(sq_selected)   # Add first and second clicks
+                if len(player_clicks) == 2:             # If this is the second click, make move if possible
+                    move = chess_engine.Move(player_clicks[0], player_clicks[1], game_state.board)
+                    print(move.get_chess_not())
+                    game_state.make_move(move)
+                    sq_selected = ()                    # Reset play clicks
+                    player_clicks = []
+
         draw_game_state(screen, game_state)
         clock.tick(MAX_FPS)
         p.display.flip()
 
 
 def draw_game_state(screen, game_state):
-    draw_board(screen)      #Draw the board
+    draw_board(screen)      # Draw the board
     draw_pieces(screen, game_state.board)
 
 
